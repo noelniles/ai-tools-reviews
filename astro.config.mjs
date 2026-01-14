6395d7ba53df54fad8806cbb78a805bd3dec3cb5
Zane Merrik <zane@benchthebots.ai>
2026-01-13 16:59:55 -1000

    Add SEO: robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://benchthebots.ai',
   integrations: [
     tailwind({
       configFile: './tailwind.config.mjs'
     }),
-    mdx()
+    mdx(),
+    sitemap()
   ]
 })
